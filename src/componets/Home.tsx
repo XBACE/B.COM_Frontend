7d671a74a495d7137e6e8938343bc004dd16d276
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-01-19 17:42:54 +0200

    new landing page

---
+++ src/componets/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+    <main style={{backgroundImage:"url('assets/background.jpg')"}} className="flex jutsify-center bg-[#0569B6]">
+        <section></section>
+        <section></section>
+    </main>
+    </>
+
+  )
+}
+
+export default Home
